849f1c543e12ca842962e004113fd725d5b7789a
Juliandav2 <mezka28@gmail.com>
2026-02-25 16:03:39 -0500

    refactor(repository) : rename InMemoryRepository to InMemoryOrderRepository

--- src/main/java/com/tienda/repository/InMemoryOrderRepository.java
+++ src/main/java/com/tienda/repository/InMemoryOrderRepository.java
@@ -4,10 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * In-memory implementation of {@link OrderRepository}.
+ *
+ * <p>
+ * This repository stores orders in a HashMap for testing
+ * and development purposes.
+ * </p>
+ *
+ * <p>
+ * It is not intended for production use, as data will be lost
+ * when the application stops.
+ * </p>
+ */
 
 public class InMemoryRepository implements OrderRepository {
 
     private final Map<String, Order> storage = new HashMap<>();
+
+    /**
+     * Saves or replaces the given order in memory.
+     *
+     * @param order the order to store
+     */
 
     @Override
     public void save (Order order) {
