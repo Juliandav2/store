6c85dde857a113c946426f5c24583f4f2836c247
Juliandav2 <mezka28@gmail.com>
2026-03-06 12:10:50 -0500

    feat(config): externaliza database and JWT configuration with environmet variables

--- src/main/java/com/tienda/security/JwtService.java
+++ src/main/java/com/tienda/security/JwtService.java
@@ -2,25 +2,30 @@ package com.tienda.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.security.Key;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private static final String SECRET = "miClaveSecretaSuperSeguraParaJWT1234567890ABCDEFGHIJ";
-    private static final long EXPIRATION = 1000 * 60 * 60 * 24; // 24 hours
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
 
     private SecretKey getKey () {
-        return Keys.hmacShaKeyFor(SECRET.getBytes());
+        return Keys.hmacShaKeyFor(secret.getBytes());
     }
 
     public String generateToken (String username) {
         return Jwts.builder().subject(username)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getKey()).compact();
     }
 
